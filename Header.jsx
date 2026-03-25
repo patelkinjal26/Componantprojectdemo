5371955efcd1a56b7eddbe9929d8f35bfde9cdb8
Patel Kinjal <patelkinjal2617@gmail.com>
2026-03-25 13:09:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2 style={{backgroundColor:'lightpink',color:'white'}}>
+                    Patel Kinjal
+                </h2>
+            </div>
+        ) 
+    }
+}
+export default Header;
